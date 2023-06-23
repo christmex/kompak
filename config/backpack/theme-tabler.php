2a3403b2772d5698bfe2e2294070d86e9fc9475f
christmex <christmex@yahoo.com>
2023-06-23 20:16:09 +0700

    First commit

---
+++ config/backpack/theme-tabler.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /**
+     * 1st layer of customization
+     *
+     * Simple pick a layout and let Backpack decide the best look for it.
+     * No extra step is required.
+     *
+     * Possible values: horizontal, horizontal_dark, horizontal_overlap, vertical,
+     * vertical_dark, vertical_transparent (legacy theme), right_vertical, right_vertical_dark, right_vertical_transparent
+     */
+    'layout' => 'horizontal',
+
+    /**
+     * Pick a login page layout.
+     * Possible values: default, illustration, cover
+     */
+    'auth_layout' => 'default', // default, illustration, cover
+
+];
